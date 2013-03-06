1828d49c5f7e03fcb5faf1a430cf30ed92578074
Ethan Mick <ethan@cloudmine.me>
2013-03-06 14:13:37 -0500

    Added Xcode ASDelaunay project

---
+++ ASDelaunayTests/ASDelaunayTests.h
@@ -0,0 +1,13 @@
+//
+//  ASDelaunayTests.h
+//  ASDelaunayTests
+//
+//  Created by Ethan Mick on 3/6/13.
+//  Copyright (c) 2013 Ethan Mick. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ASDelaunayTests : SenTestCase
+
+@end
---
+++ ASDelaunayTests/ASDelaunayTests.m
@@ -0,0 +1,32 @@
+//
+//  ASDelaunayTests.m
+//  ASDelaunayTests
+//
+//  Created by Ethan Mick on 3/6/13.
+//  Copyright (c) 2013 Ethan Mick. All rights reserved.
+//
+
+#import "ASDelaunayTests.h"
+
+@implementation ASDelaunayTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in ASDelaunayTests");
+}
+
+@end
