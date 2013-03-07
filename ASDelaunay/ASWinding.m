0adfaf6ee6d10327f4b786f27a22b91d2d010299
Ethan Mick <ethanmski@gmail.com>
2013-03-06 21:41:37 -0500

    Adding ASWinding.

---
+++ ASDelaunay/ASWinding.m
@@ -0,0 +1,13 @@
+//
+//  ASWinding.m
+//  ASDelaunay
+//
+//  Created by Ethan Mick on 3/6/13.
+//  Copyright (c) 2013 Ethan Mick. All rights reserved.
+//
+
+#import "ASWinding.h"
+
+@implementation ASWinding
+
+@end
